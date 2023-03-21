7b53120752164569f5788b5807b0bb0e1ae64065
delizin <delizin@gmail.com>
2023-03-20 20:22:06 -0400

    Update config.lua

--- Lua/config/config.lua
+++ Lua/config/config.lua
@@ -53,6 +53,19 @@ config.PointsGainedFromSkill = {
     helm = 9,
 }
 
+----- Skill Loss on Death -----
+-- Number of permanent skill points to lose on death, 0 to disable
+config.deathSkillLossFlat = 0
+-- Percentage of skills to lose on death, 0 means none, 100 means all
+-- Can be combined with flat loss
+config.deathSkillLossPercentage = 5.0
+-- Can be used to create conditions like "loose 10% of your skill but at least 3 and at most 5"
+config.deathSkillLossMin = 0
+config.deathSkillLossMax = 100
+-- Whether to respect starting skill minimum. Deactivation allows skills to drop to 0.
+config.respectSkillMin = true
+
+
 config.PointsLostAfterNoLives = function (x)
     return x * 0.75
 end
