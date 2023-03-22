ef475bb4c7e979d5597d13225b21caa2ccd0a448
delizin <delizin@gmail.com>
2023-03-21 22:21:35 -0400

    Update config.lua

--- Lua/config/config.lua
+++ Lua/config/config.lua
@@ -21,6 +21,8 @@ config.OptionalTraitors = true        -- players can use !toggletraitor
 config.RagdollOnDisconnect = false
 config.EnableControlHusk = false     -- EXPERIMENTAL: enable to control husked character after death
 
+config.TraitorChance = 0.15
+
 -- This overrides the game's respawn shuttle, and uses it as a submarine injector, to spawn submarines in game easily. Respawn should still work as expected, but the shuttle submarine file needs to be manually added here.
 -- Note: If this is disabled, traitormod will disable all functions related to submarine spawning.
 -- Warning: Only respawn shuttles will be used, the option to spawn people directly into the submarine doesnt work.
